d5150397c35a937d16ac1f1344007c8fd94ecb9a
Celia Gómez de Villavedón Pedrosa <celiagomezdev@gmail.com>
2017-06-23 20:57:23 +0200

    Initial Commit

---
+++ Vivid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vivid
+//
+//  Created by Celia Gómez de Villavedón on 23/06/2017.
+//  Copyright © 2017 Celia Gómez de Villavedón Pedrosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
