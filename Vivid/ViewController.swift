2b427c70a6033d581fcae7d457b724792500c4d2
Celia Gómez de Villavedón Pedrosa <celiagomezdev@gmail.com>
2017-06-24 15:33:43 +0200

    Fix issue with Map: center it in Berlin

--- Vivid/ViewController.swift
+++ Vivid/ViewController.swift
@@ -14,22 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var toolBar: UIToolbar!
     @IBOutlet weak var mapView: GMSMapView!
- 
+    
+    var googleMap: GMSMapView?
+
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
-        let camera = GMSCameraPosition.camera(withLatitude: 52.520736, longitude: 13.409423, zoom: 15)
-        let map = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        self.mapView = map
+        let camera = GMSCameraPosition.camera(withLatitude: 52.520736, longitude: 13.409423, zoom: 12)
+        googleMap = GMSMapView.map(withFrame: mapView.frame, camera: camera)
+        self.mapView = googleMap
+        self.view.addSubview(mapView!)
         
         let initialLocation = CLLocationCoordinate2DMake(52.520736, 13.409423)
         let marker = GMSMarker(position: initialLocation)
         marker.title = "Berlin"
+        marker.map = mapView
         
     }
-    
+
     
     @IBAction func search(_ sender: Any) {
+
     }
  }
 
