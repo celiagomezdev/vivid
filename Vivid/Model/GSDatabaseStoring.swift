c8ee23f1c43543dbaca282038b7a83a2712bb431
Celia Gómez de Villavedón Pedrosa <celiagomezdev@gmail.com>
2017-08-25 15:31:11 +0200

    Fix problems with folders

---
+++ Vivid/Model/GSDatabaseStoring.swift
@@ -0,0 +1,94 @@
+//
+//  GSDatabaseStoring.swift
+//  Vivid
+//
+//  Created by Celia Gómez de Villavedón on 23/08/2017.
+//  Copyright © 2017 Celia Gómez de Villavedón Pedrosa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+import Sync
+
+class GSDatabaseStoring: NSObject {
+    
+    let dataStack = DataStack(modelName: "NonSmokingBarModel")
+    var nonSmokingBars = [NonSmokingBar]()
+    var managedObjectContext: NSManagedObjectContext!
+    let request = NSFetchRequest<NSFetchRequestResult>(entityName: "NonSmokingBar")
+    var newManagedObjectContext: NSManagedObjectContext!
+    
+    override init() {
+        super.init()
+    }
+    
+    //MARK: Sync data into Model from a JSON file (Google Sheets Database)
+    func importFromJSON() {
+        
+        self.getDataWith { (json, error) in
+            
+            guard error == nil else { print("Could not import the JSON to NonSmoking barModel"); return }
+            
+            if let jsonResult = json?["results"] as? [[String:Any]] {
+                
+                self.dataStack.sync(jsonResult, inEntityNamed: "NonSmokingBar") { error in
+                    guard error == nil else { print("Could not import the JSON to NonSmoking barModel"); return }
+                    print("SAVED \(jsonResult.count) in core data")
+                }
+                
+            } else {
+                print("Could not get data as [[String:Any]]")
+            }
+        }
+        
+        print("importListData called")
+        
+    }
+    
+    //MARK: Helpers
+    
+    //Parse data from JSON file
+    func getDataWith(completion: @escaping (_ result: AnyObject?,_ error: NSError?) -> Void) {
+        
+        guard let url = URL(string: GMSClient.Constants.myListURL) else { return }
+        print(url)
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard error == nil else {
+                completion(nil, error! as NSError)
+                print("Could not receive the data from NonSmokingBars Sheet: \(String(describing: error))")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data")
+                return
+            }
+            var parsedResult: AnyObject! = nil
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+                
+                DispatchQueue.main.async {
+                    print("JSON data sent to completion handler)")
+                    completion(parsedResult, nil)
+                }
+            } catch {
+                let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+                completion(nil, NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
+                
+            }
+            }.resume()
+    }
+    
+
+    // MARK: Shared Instance
+    
+    class func sharedInstance() -> GSDatabaseStoring {
+        struct Singleton {
+            static var sharedInstance = GSDatabaseStoring()
+        }
+        return Singleton.sharedInstance
+    }
+}
---
+++ Vivid/Model/Model.swift
@@ -0,0 +1,254 @@
+//
+//  Model.swift
+//  Vivid
+//
+//  Created by Celia Gómez de Villavedón on 07/08/2017.
+//  Copyright © 2017 Celia Gómez de Villavedón Pedrosa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+import Sync
+
+
+class Model: NSObject {
+    
+    let dataStack = DataStack(modelName: "NonSmokingBarModel")
+    var nonSmokingBars = [NonSmokingBar]()
+    var managedObjectContext: NSManagedObjectContext!
+    let request = NSFetchRequest<NSFetchRequestResult>(entityName: "NonSmokingBar")
+    var newManagedObjectContext: NSManagedObjectContext!
+    
+    override init() {
+        super.init()
+    }
+    
+    
+   //Load results in an array
+    
+    func loadData() {
+        
+        managedObjectContext = dataStack.viewContext
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            nonSmokingBars = try managedObjectContext.fetch(request) as! [NonSmokingBar]
+            print("Nº in Array: \(nonSmokingBars.count)")
+            
+            for result in nonSmokingBars {
+                if let name = result.name {
+                    print("Name: \(name)")
+                }
+                if let address = result.address {
+                    print("Address: \(address)")
+                }
+                if let location = result.location {
+                    print("Location: \(location)")
+                }
+                if let neighbourhood = result.neighbourhood {
+                    print("Neighbourhood: \(neighbourhood)")
+                }
+                
+                print("Postal Code: \(result.postalCode)")
+                
+                print("Rating: \(result.rating)")
+                
+                if let placeID = result.placeId {
+                    print("Place ID: \(placeID)")
+                }
+    
+                if let checked = result.checked {
+                    print("Checked: \(checked)")
+                }
+                
+                if let smokingType = result.smokingType {
+                    print("SmokingType: \(smokingType)")
+                }
+                
+                if let photos = result.photos {
+                    if let photosStArray = NSKeyedUnarchiver.unarchiveObject(with: photos as Data) as? [String] {
+                        print("Photos: \(photosStArray)")
+                    } else {
+                        print("Could not extract Photos from Model")
+                    }
+                 }
+                
+                if let placeTypes = result.placeTypes {
+                    if let placeTypesStArray = NSKeyedUnarchiver.unarchiveObject(with: placeTypes as Data) as? [String] {
+                        print("Photos: \(placeTypesStArray)")
+                    } else {
+                        print("Could not extract Place Types from Model")
+                    }
+                }
+            }
+        } catch {
+            print("Could not load data from database: \(error.localizedDescription)")
+        }
+    }
+    
+    func addData() {
+        
+        managedObjectContext = dataStack.viewContext
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try managedObjectContext.fetch(request)
+            print("nº in Database: \(results.count)")
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let barName = result.value(forKey: "name") as? String {
+                        
+                        switch barName {
+                            
+                        case "K-Fetisch":
+                            print("SAVED place id for bar: \(barName)")
+                            result.setValue("ChIJwbcY56VPqEcRWeH0D3fTHf0", forKey: "placeId")
+                        case "Café Pförtner":
+                            print("SAVED place id for bar: \(barName)")
+                            result.setValue("ChIJI-j0Uy9SqEcRPfHhhUyfajo", forKey: "placeId")
+                        case "Mano":
+                            print("SAVED place id for bar: \(barName)")
+                            result.setValue("ChIJQwk7-kpOqEcRZwckKwSnWq0", forKey: "placeId")
+                        case "Ungeheuer":
+                            print("SAVED place id for bar: \(barName)")
+                            result.setValue("ChIJfT45VplPqEcRjouZhssZ7M8", forKey: "placeId")
+                        case "Wolf Kino":
+                            print("SAVED place id for bar: \(barName)")
+                            result.setValue("ChIJFyPA2KVPqEcRTNkgWcCNCHg", forKey: "placeId")
+                        case "Hops & Barley":
+                            print("SAVED place id for bar: \(barName)")
+                            result.setValue("ChIJbQ8FyVhOqEcRWwWTSlK_3W4", forKey: "placeId")
+                        case "Laika":
+                            print("SAVED place id for bar: \(barName)")
+                            result.setValue("ChIJl86BkJ5PqEcRwLxtNiNWZMo", forKey: "placeId")
+                        default:
+                            print("We couldn't set the place id for bar: \(barName)")
+                        }
+                        
+                        do {
+                            try managedObjectContext.save()
+                            print("PLACE ID UPDATED")
+                        } catch {
+                            print("We could not save correctly the PLACE ID into context")
+                        }
+                    }
+                }
+            }
+        }  catch {
+            print("We couldn't save correctly the data into context")
+        }
+    }
+
+    func updateData() {
+        
+        managedObjectContext = dataStack.viewContext
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try managedObjectContext.fetch(request)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let barName = result.value(forKey: "name") as? String {
+                        
+                        if barName == "Laika " {
+                            
+                            result.setValue("Laika", forKey: "name")
+                            
+                            do {
+                                try managedObjectContext.save()
+                                print("UPDATED")
+                            } catch {
+                                print("We could not save correctly the PLACE ID into context")
+                            }
+                        }
+                    }
+                }
+            }
+        } catch {
+            print("We couldn't save correctly the data into context")
+        }
+    }
+
+    // Delete entry
+    func deleteData() {
+        
+        managedObjectContext = dataStack.viewContext
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try managedObjectContext.fetch(request)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let barName = result.value(forKey: "name") as? String {
+                        
+                        if barName == "Kptn A. Müller" {
+                            print("Object deleted: \(result)")
+                            managedObjectContext.delete(result)
+                        }
+                        
+                        do {
+                            try managedObjectContext.save()
+                        } catch {
+                            print("We could not save correctly the PLACE ID into context")
+                        }
+                    }
+                }
+            }
+        } catch {
+            print("We couldn't save correctly the data into context")
+        }
+    }
+    
+    
+    //MARK: Export JSON from model - ERROR: I cannot get a json object
+    func exportDataAsJSON () {
+        
+        managedObjectContext = dataStack.viewContext
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try managedObjectContext.fetch(request)
+            
+            if results.count > 0 {
+                
+                for entry in results as! [NSManagedObject] {
+                    
+                    let entriesValues = entry.export()
+                    print(entriesValues)
+                    
+                }
+            }
+        } catch {
+            print("We could not fetch the data")
+        }
+    }
+    
+
+    // MARK: Shared Instance
+
+    class func sharedInstance() -> Model {
+        struct Singleton {
+            static var sharedInstance = Model()
+        }
+        return Singleton.sharedInstance
+    }
+}
